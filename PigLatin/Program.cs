c3a4da521c906705e4b8ba1b151b4756deebdf04
Zanros421 <michaelhoffmann744@gmail.com>
2019-05-08 19:11:15 -0500

    Did it work?

---
+++ PigLatin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void PigLatin()
+        {
+            Console.WriteLine("Give me a word or sentence to convert to to Pig Latin.");
+            string word = Console.ReadLine().ToLower();
+            if (word)
+            {
+
+            }
+        }
+    }
+}
